baaf63ebcfbdab499b0bc23b8b59b3bc34e1f640
Inon Bercu <inonbr@gmail.com>
2022-06-08 13:01:24 +0300

    created some routers

---
+++ posts/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const postsRouter = require("./routers/postsRouters");
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/posts", postsRouter);
+
+app.listen(port, () => {
+  console.log(`🟢 app listening on port http://localhost:${port}`);
+});
---
+++ posts/routers/postsRouters.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = new express.Router();
+const { v4: uuidv4 } = require("uuid");
+const { check, validationResult } = require("express-validator");
+
+// we will not use a database for this project
+const posts = {};
+
+router.get("/", (req, res) => {
+  res.send(posts);
+});
+
+router.post(
+  "/",
+  [check("title", "title is required").trim().not().isEmpty()],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title } = req.body;
+    const id = uuidv4();
+    const newPost = { id, title };
+
+    posts.id = newPost;
+
+    res.status(201).send({ ...newPost, msg: "post created" });
+  }
+);
+
+module.exports = router;
